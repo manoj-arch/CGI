ff6df9c26202e2ce3facf6bfa3672d18c3f95a0e
manoj-arch <manoj92067@gmail.com>
2024-06-03 18:48:16 +0530

    commit changes

--- Even.java
+++ Even.java
@@ -1,6 +1,6 @@
 public class Even {
     public static void main(String[] args) {
-        int num=11;
+        int num=10;
 
         if(num % 2==0){
             System.out.println("This is Even number");
